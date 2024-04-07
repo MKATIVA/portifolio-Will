54a2f979a2f82d7d71fddd1f017010c8ab100e11
MARCOS KENNED <escolademusicamk@gmail.com>
2024-04-07 17:36:20 -0300

    Atualizar

---
+++ carousel.js
@@ -0,0 +1,27 @@
+/** @format */
+
+$(document).ready(function () {
+  $(".card").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    swipeToSlide: true,
+    arrows: true, 
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
